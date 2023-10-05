2c82d03babac52574a7b07ab9a0be298d906b9d1
berkinkara <25124833+berkinkara@users.noreply.github.com>
2023-10-05 17:18:36 +0200

    replace colon with semicolon and reorder arrow keys

--- keymap.c
+++ keymap.c
@@ -16,7 +16,7 @@ enum layer_names {
 #define RSFT_J RSFT_T(KC_J)
 #define RCTL_K RCTL_T(KC_K)
 #define RALT_L RALT_T(KC_L)
-#define RGUI_M RGUI_T(KC_M)
+#define RGUI_SCLN RGUI_T(KC_SCLN)
 
 #define SYM_BSPC LT(SYMBOL, KC_BSPC)
 #define FNC_ENT LT(FUNCTION, KC_ENT)
@@ -31,21 +31,20 @@ enum layer_names {
 #define RSFT_UNDS RSFT_T(KC_UNDS)
 #define RCTL_DQUO RCTL_T(KC_DQUO)
 #define RALT_QUOT RALT_T(KC_QUOT)
-#define RGUI_COLN RGUI_T(KC_COLN)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_split_3x5_2(
         KC_Q      , KC_W      , KC_E      , KC_R      , KC_T      ,        KC_Y      , KC_U      , KC_I      , KC_O      , KC_P      ,
-        LGUI_A    , LALT_S    , LCTL_D    , LSFT_F    , KC_G      ,        KC_H      , RSFT_J    , RCTL_K    , RALT_L    , RGUI_M    ,
+        LGUI_A    , LALT_S    , LCTL_D    , LSFT_F    , KC_G      ,        KC_H      , RSFT_J    , RCTL_K    , RALT_L    , RGUI_SCLN ,
         KC_Z      , KC_X      , KC_C      , KC_V      , KC_B      ,        KC_N      , KC_M      , KC_COMM   , KC_DOT    , KC_SLSH   ,
                                             MED_TAB   , NAV_SPC   ,        SYM_BSPC  , FNC_ENT
     ),
     [SYMBOL] = LAYOUT_split_3x5_2(
         KC_1      , KC_2      , KC_3      , KC_4      , KC_5      ,        KC_6      , KC_7      , KC_8      , KC_9      , KC_0      ,
-        LGUI_LGRV , LALT_LCBR , LCTL_LBRC , LSFT_LPRN , KC_PLUS   ,        KC_ASTR   , RSFT_UNDS , RCTL_DQUO , RALT_QUOT , RGUI_COLN ,
+        LGUI_LGRV , LALT_LCBR , LCTL_LBRC , LSFT_LPRN , KC_PLUS   ,        KC_ASTR   , RSFT_UNDS , RCTL_DQUO , RALT_QUOT , RGUI_SCLN ,
         KC_BSLS   , KC_RCBR   , KC_RBRC   , KC_RPRN   , KC_MINS   ,        KC_SLSH   , KC_EQL    , KC_LT     , KC_GT     , KC_QUES   ,
-                                            KC_ESC    , RGUI_COLN ,        XXXXXXX   , XXXXXXX
+                                            XXXXXXX   , KC_ESC    ,        XXXXXXX   , XXXXXXX
     ),
     [FUNCTION] = LAYOUT_split_3x5_2(
         KC_F1     , KC_F2     , KC_F3     , KC_F4     , KC_F5     ,        KC_F6     , KC_F7     , KC_F8     , KC_F9     , KC_F10    ,
@@ -55,14 +54,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [NAVIGATION] = LAYOUT_split_3x5_2(
         KC_ESC    , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,        KC_COPY   , KC_UNDO   , KC_INS    , XXXXXXX   , KC_DEL    ,
-        KC_LGUI   , KC_LALT   , KC_LCTL   , KC_LSFT   , XXXXXXX   ,        XXXXXXX   , KC_LEFT   , KC_DOWN   , KC_UP     , KC_RGHT   ,
-        KC_CAPS   , KC_CUT    , KC_COPY   , KC_PSTE   , QK_BOOT   ,        XXXXXXX   , KC_HOME   , KC_PGDN   , KC_PGUP   , KC_END    ,
+        KC_LGUI   , KC_LALT   , KC_LCTL   , KC_LSFT   , XXXXXXX   ,        KC_LEFT   , KC_DOWN   , KC_UP     , KC_RGHT   , XXXXXXX   ,
+        KC_CAPS   , KC_CUT    , KC_COPY   , KC_PSTE   , QK_BOOT   ,        KC_HOME   , KC_PGDN   , KC_PGUP   , KC_END    , XXXXXXX   ,
                                             XXXXXXX   , XXXXXXX   ,        XXXXXXX   , XXXXXXX
     ),
     [MEDIA] = LAYOUT_split_3x5_2(
         XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,        XXXXXXX   , XXXXXXX   , KC_MRWD   , KC_MFFD   , XXXXXXX   ,
-        XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,        XXXXXXX   , KC_MPLY   , KC_VOLD   , KC_VOLU   , KC_MUTE   ,
-        XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,        XXXXXXX   , XXXXXXX   , KC_BRID   , KC_BRIU   , XXXXXXX   ,
+        XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,        KC_MPLY   , KC_VOLD   , KC_VOLU   , KC_MUTE   , XXXXXXX   ,
+        XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   , XXXXXXX   ,        XXXXXXX   , KC_BRID   , KC_BRIU   , XXXXXXX   , XXXXXXX   ,
                                             XXXXXXX   , XXXXXXX   ,        XXXXXXX   , XXXXXXX
     ),
 };
