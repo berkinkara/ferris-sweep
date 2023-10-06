014508b389db9a91c6ebdc020a159e24cd963650
berkinkara <25124833+berkinkara@users.noreply.github.com>
2023-10-06 14:49:07 +0200

    configure tap dance keys

--- keymap.c
+++ keymap.c
@@ -1,5 +1,7 @@
 #include QMK_KEYBOARD_H
 
+#include "tap_dance.h"
+
 enum layer_names {
     BASE,
     SYMBOL,
@@ -35,9 +37,9 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_split_3x5_2(
-        KC_Q      , KC_W      , KC_E      , KC_R      , KC_T      ,        KC_Y      , KC_U      , KC_I      , KC_O      , KC_P      ,
-        LGUI_A    , LALT_S    , LCTL_D    , LSFT_F    , KC_G      ,        KC_H      , RSFT_J    , RCTL_K    , RALT_L    , RGUI_SCLN ,
-        KC_Z      , KC_X      , KC_C      , KC_V      , KC_B      ,        KC_N      , KC_M      , KC_COMM   , KC_DOT    , KC_SLSH   ,
+        TD(Q_ESC) , KC_W      , KC_E      , KC_R      , KC_T      ,        KC_Y      , KC_U      , KC_I      , KC_O      , KC_P             ,
+        LGUI_A    , LALT_S    , LCTL_D    , LSFT_F    , KC_G      ,        KC_H      , RSFT_J    , RCTL_K    , RALT_L    , TD(SCLN_RGUI_CLN),
+        TD(Z_CAPS), KC_X      , KC_C      , KC_V      , KC_B      ,        KC_N      , KC_M      , KC_COMM   , KC_DOT    , KC_SLSH          ,
                                             MED_TAB   , NAV_SPC   ,        SYM_BSPC  , FNC_ENT
     ),
     [SYMBOL] = LAYOUT_split_3x5_2(
---
+++ tap_dance.h
@@ -0,0 +1,10 @@
+#ifndef FERRIS_SWEEP_TAP_DANCE_H 
+#define FERRIS_SWEEP_TAP_DANCE_H
+
+enum {
+    Q_ESC,
+    SCLN_RGUI_CLN,
+    Z_CAPS,
+};
+
+#endif
